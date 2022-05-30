43faf466c6a07e843450fe2813810675cd01baa9
Bishop Shaw III <prototype124@yahoo.com>
2022-05-30 00:14:20 -0700

    admin index routes

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -13,8 +13,8 @@ import { index, login, post, logout } from "../../controllers/admin/admin.js";
 adminRouter.get("/dashboard", checkAuthentication, index);
 adminRouter.get("/", login);
 adminRouter.post(
-  "/login",
-  passport.authenticate("local", { failureRedirect: "/login" }),
+  "/",
+  passport.authenticate("local", { failureRedirect: "/" }),
   post,
 );
 adminRouter.get("/logout", logout);
