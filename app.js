3a137fb453ccd5ee198ff09154f9a5db52987d49
Bishop Shaw III <prototype124@yahoo.com>
2022-06-27 20:08:24 -0700

    app.js edit

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-import { config } from "dotenv";
 if (process.env.NODE_ENV !== "production") {
-  config();
+  require("dotenv").config();
 }
 
-import express from "express";
+const express = require("express");
 const app = express();
-import bodyParser from "body-parser";
-import methodOverride from "method-override";
-import path from "path";
-import { fileURLToPath } from "url";
-import session from "express-session";
-import passport from "passport";
-import LocalStrategy from "passport-local";
-import User from "./models/AdminUsers.js";
-
-//replicate the functionality of __dirname - es6 module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-import "./mongoDatabase.js";
-//require("./utility/error");
-
+require("./mongoDatabase.js");
+require("./utility/error");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const path = require("path");
+//const { fileURLToPath } = require "url";
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/AdminUsers");
 const PORT = process.env.PORT || 5000;
 
-// admin routes
-import adminIndexRoutes from "./routes/admin/index.js";
-import adminArticleRoutes from "./routes/admin/articles.js";
-import adminProductRoutes from "./routes/admin/products.js";
-import adminContactRoutes from "./routes/admin/contact.js";
-import adminUserRoutes from "./routes/admin/users.js";
+//replicate the functionality of __dirname - es6 module
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-//const errorRoutes = require("./utility/error");
+// admin routes
+const adminIndexRoutes = require("./routes/index");
+const adminArticleRoutes = require("./routes/articles");
+const adminProductRoutes = require("./routes/products");
+const adminContactRoutes = require("./routes/contact");
+const adminUserRoutes = require("./routes/users");
+
+const errorRoutes = require("./utility/error");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.use(express.static("public"));
-app.use(express.json());
 app.use(methodOverride("_method"));
 app.use(
   session({
@@ -42,7 +42,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       secure: false,
-      maxAge: 3600000, //1 hour
+      maxAge: 1000 * 60 * 60 * 24 * 7,
     },
   }),
 );
@@ -51,23 +51,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
 // admin middleware
+app.use("/", adminIndexRoutes);
 app.use("/articles", adminArticleRoutes);
 app.use("/articles/:slug/products", adminProductRoutes);
 app.use("/messages", adminContactRoutes);
 
-app.use("/", adminIndexRoutes, adminUserRoutes);
-
 // error middleware
-// app.use(errorRoutes.errorMessage);
-// app.use(errorRoutes.AsyncError);
+app.use(errorRoutes.errorMessage);
+app.use(errorRoutes.AsyncError);
 
 app.listen(PORT, () => console.log(`Server on PORT ${PORT}`));
