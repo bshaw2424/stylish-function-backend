4ecef2bf1459fd3c92cfcd49fa6e4d9e879b90e5
Bishop Shaw III <prototype124@yahoo.com>
2022-06-30 15:13:02 -0700

    functions.js file

--- functions.js
+++ functions.js
@@ -3,3 +3,11 @@
 module.exports.convertPriceWithComma = price => {
   return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+module.exports.convertEmailToLowerCase = email => {
+  const splitEmailCharacters = email.split("");
+  const emailCharacters = splitEmailCharacters.map(characters => {
+    return characters.toUpperCase();
+  });
+  return emailCharacters.join("");
+};
