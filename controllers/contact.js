5ee608e4c8a1eb4f4ee1a28bf3519d9b3e26f32f
Bishop Shaw III <prototype124@yahoo.com>
2022-08-29 19:05:23 -0700

    contact.js file

--- controllers/contact.js
+++ controllers/contact.js
@@ -45,14 +45,56 @@ module.exports.index = async (req, res) => {
   // }
 
   const messages = await ContactModel.find({}).sort({ created_on: -1 });
-
   res.render("admin/contacts/contact", {
     messages,
     contactLengthMessage: "All Clear...No Messages",
   });
 };
 
-module.exports.post = async (req, res) => {};
+module.exports.post = async (req, res) => {
+  const { Messages } = req.body;
+  console.log(Messages);
+  const response = await fetch("/messages");
+  const data = response.json();
+  console.log(data);
+  // const fetchContactsUrlfromDatabase =
+  //   "https://data.mongodb-api.com/app/data-hsqwk/endpoint/data/v1/action/find";
+
+  // const fetchConfigurationToGetContacts = {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Access-Control-Request-Headers": "*",
+  //     "api-key": process.env.MONGO_KEY,
+  //     Accept: "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     collection: "contacts",
+  //     database: "affiliate_products",
+  //     dataSource: "Cluster0",
+  //   }),
+  // };
+
+  // try {
+  //   const response = await fetch(
+  //     fetchContactsUrlfromDatabase,
+  //     fetchConfigurationToGetContacts,
+  //   );
+  //   if (!response.ok) {
+  //     throw new Error();
+  //   }
+  //   const messages = await response.json();
+  //   const { documents } = messages;
+  //   const contactDataResponse = documents;
+
+  //   res.render("admin/contacts/contact", {
+  //     contactDataResponse,
+  //     contactLengthMessage: "All Clear...No Messages",
+  //   });
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
+};
 
 module.exports.ascSort = async (req, res) => {
   const ascendingSort = await ContactModel.find({}).sort({ created_on: 1 });
